993f6e0d1ed643fa1b5535e13a2a77084137696e
Esteban Pereira <estebanpereira748@gmail.com>
2023-06-25 23:49:05 -0300

    ADD root layout

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+<<<<<<< Updated upstream
 import "./style.css";
 
 document.querySelector(
   "#app"
 ).innerHTML = `<div class="text-red-300 h-96 w-full bg-red-500">Hello World</div>`;
+=======
+//document.querySelector("#app").innerHTML = ``;
+>>>>>>> Stashed changes
